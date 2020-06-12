8d519e970bc85ed4328d8fed32979fdcff87faee
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-06-12 19:10:13 +0300

    Update get.py

--- get.py
+++ get.py
@@ -21,7 +21,7 @@ args = parser.parse_args()
 
 if args.devicename:
     # Get data from database, searching with ID inserted while executing
-    for output in database.execute(f"SELECT deviceName FROM device_settings WHERE ID = '{args.devicename}' ):
+    for output in database.execute(f"SELECT deviceName FROM device_settings WHERE ID = '{args.devicename}'"):
         # Convert name to list
         output = output[0]
         # Print search result
