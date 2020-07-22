9be716f07e797f452a943be810e5beb243edf139
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-07-23 00:31:38 +0300

    Create rest controller for controlling main body, moved javascript scripts to new folder "js"

---
+++ src/main/kotlin/com/ggoraa/glados/core/controller/rest/MainController.kt
@@ -0,0 +1,31 @@
+package com.ggoraa.glados.core.controller.rest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+    @GetMapping("/rest/move/head/{xAxis}/{yAxis}")
+    fun controlHead(
+            @PathVariable yAxis: Int,
+            @PathVariable xAxis: Int
+    ) {
+
+    }
+
+    @GetMapping("/rest/move/body/{xAxis}/{yAxis}")
+    fun controlBody(
+            @PathVariable yAxis: Int,
+            @PathVariable xAxis: Int
+    ) {
+
+    }
+
+    @GetMapping("/rest/move/baseTurn/{turnSpeed}")
+    fun controlBase(
+            @PathVariable turnSpeed: String
+    ) {
+
+    }
+}
